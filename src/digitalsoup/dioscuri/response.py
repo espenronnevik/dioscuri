b195188c2ab09ab836d2c17d02e7cd598a80a1b7
Espen Rønnevik <espen@digitalsoup.eu>
2024-06-21 17:48:04 +0200

    Combine the _replymsg and _replybody methods into _reply

--- src/digitalsoup/dioscuri/response.py
+++ src/digitalsoup/dioscuri/response.py
@@ -19,40 +19,40 @@ class Response:
         self.statuscode = None
         self.data = None
 
-    def _replymsg(self, message):
-        if message is None:
+    def _reply(self, info, data=None):
+        if data is not None:
+            return f"{self.statuscode} {info} {CRLF}".encode() + data
+        elif info is None:
             return f"{self.statuscode} {CRLF}".encode()
-        return f"{self.statuscode} {message} {CRLF}".encode()
-
-    def _replybody(self, mimetype, body):
-        return f"{self.statuscode} {mimetype} {CRLF}".encode() + body
+        else:
+            return f"{self.statuscode} {info} {CRLF}".encode()
 
     def input(self, prompt, statusdigit=0):
         self.statustype = StatusType.INPUT
         self.statuscode = 10 + statusdigit
-        self.data = self._replymsg(prompt)
+        self.data = self._reply(prompt)
 
     def success(self, mimetype, body, statusdigit=0):
         self.statustype = StatusType.SUCCESS
         self.statuscode = 20 + statusdigit
-        self.data = self._replybody(mimetype, body)
+        self.data = self._reply(mimetype, body)
 
     def redirect(self, uri, statusdigit=0):
         self.statustype = StatusType.REDIRECT
         self.statuscode = 30 + statusdigit
-        self.data = self._replymsg(uri)
+        self.data = self._reply(uri)
 
     def tempfail(self, statusdigit=0, message=None):
         self.statustype = StatusType.TEMPFAIL
         self.statuscode = 40 + statusdigit
-        self.data = self._replymsg(message)
+        self.data = self._reply(message)
 
     def permfail(self, statusdigit=0, message=None):
         self.statustype = StatusType.PERMFAIL
         self.statuscode = 50 + statusdigit
-        self.data = self._replymsg(message)
+        self.data = self._reply(message)
 
     def auth(self, statusdigit=0, message=None):
         self.statustype = StatusType.AUTH
         self.statuscode = 60 + statusdigit
-        self.data = self._replymsg(message)
+        self.data = self._reply(message)
